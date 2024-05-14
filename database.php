d7dc6f416dd4dd3f48719c2672fd3dda3a404a71
Yosepfall <146046336+Yosepfall@users.noreply.github.com>
2024-05-14 10:17:48 +0700

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Koneksi ke database
-$koneksi = new mysqli('localhost', 'root', '', 'Toko');
-
-// Cek koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
-}
-?>
